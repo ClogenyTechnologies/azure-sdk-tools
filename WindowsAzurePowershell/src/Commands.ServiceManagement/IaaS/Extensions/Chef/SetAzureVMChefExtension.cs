fa1a0e2516601c961b0e5a13de5081534980e019
siddheshwar-more <siddheshwar.more@clogeny.com>
2014-05-15 17:23:37 +0000

    Added changes for SetAzureVMChefExtension cmdlet

--- WindowsAzurePowershell/src/Commands.ServiceManagement/IaaS/Extensions/Chef/SetAzureVMChefExtension.cs
+++ WindowsAzurePowershell/src/Commands.ServiceManagement/IaaS/Extensions/Chef/SetAzureVMChefExtension.cs
@@ -15,6 +15,8 @@
 namespace Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS.Extensions
 {
     using System.Management.Automation;
+    using System;
+    using System.IO;
     using Microsoft.WindowsAzure.Commands.ServiceManagement.Model;
 
     [Cmdlet(
@@ -26,15 +28,126 @@ namespace Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS.Extensions
     public class SetAzureVMChefExtensionCommand : VirtualMachineChefExtensionCmdletBase
     {
         protected const string SetChefExtensionParamSetName = "SetChefExtension";
+
+        [Parameter(
+            Mandatory = false,
+            Position = 2,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The Extension Version. Default is 11.12.")]
+        [ValidateNotNullOrEmpty]
+        public override string Version { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 3,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The Chef Server Validation Key File Path.")]
+        [ValidateNotNullOrEmpty]
+        public string ValidationPem { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The Chef Server Client Config (ClientRb)File Path.")]
+        [ValidateNotNullOrEmpty]
+        public string ClientRb { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The Chef Server Node Runlist.")]
+        [ValidateNotNullOrEmpty]
+        public string RunList { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The Chef Server Url.")]
+        [ValidateNotNullOrEmpty]
+        public string ChefServerUrl { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The Chef ValidationClientName, used to determine whether a chef-client may register with a Chef server.")]
+        [ValidateNotNullOrEmpty]
+        public string ValidationClientName { get; set; }
+
+        internal void ExecuteCommand()
+        {
+            ValidateParameters();
+            SetDefault();
+            SetPrivateConfig();
+            SetPublicConfig();
+            RemovePredicateExtensions();
+            AddResourceExtension();
+            WriteObject(VM);
+        }
+
+        internal void SetDefault()
+        {
+            this.Version = this.Version ?? ExtensionDefaultVersion;
+        }
+
+        internal void SetPrivateConfig()
+        {
+            this.PrivateConfiguration = string.Format(PrivateConfigurationTemplate, File.ReadAllText(this.ValidationPem).TrimEnd('\r', '\n'));
+        }
+
+        internal void SetPublicConfig()
+        {
+            string ClientConfig = string.Empty;
+
+            if (!string.IsNullOrEmpty(this.ClientRb))
+            {
+                ClientConfig = File.ReadAllText(this.ClientRb).TrimEnd('\r', '\n');
+
+                if (!string.IsNullOrEmpty(this.ChefServerUrl) && !string.IsNullOrEmpty(this.ValidationClientName))
+                {
+                    string UserConfig = @"
+chef_server_url  \""{0}\""
+validation_client_name 	\""{1}\""
+";
+                    ClientConfig += string.Format(UserConfig, this.ChefServerUrl, this.ValidationClientName);
+                }
+                else if (!string.IsNullOrEmpty(this.ChefServerUrl))
+                {
+                    string UserConfig = @"
+chef_server_url  \""{0}\""
+";
+                    ClientConfig += string.Format(UserConfig, this.ChefServerUrl);
+                }
+                else if (!string.IsNullOrEmpty(this.ValidationClientName))
+                {
+                    string UserConfig = @"
+validation_client_name 	\""{0}\""
+";
+                    ClientConfig += string.Format(UserConfig, this.ValidationClientName);
+                }
+            }
+            else if (!string.IsNullOrEmpty(this.ChefServerUrl) && !string.IsNullOrEmpty(this.ValidationClientName))
+            {
+                ClientConfig = @"
+chef_server_url  \""{0}\""
+validation_client_name 	\""{1}\""
+";
+            }
+            this.PublicConfiguration = string.Format(PublicConfigurationTemplate, ClientConfig, this.RunList);
+        }
+
         protected override void ValidateParameters()
         {
-            // ToDo
+            base.ValidateParameters();
+            if (string.IsNullOrEmpty(this.ClientRb) && (string.IsNullOrEmpty(this.ChefServerUrl) && string.IsNullOrEmpty(this.ValidationClientName)))
+            {
+                throw new ArgumentException("Required -ClientRb or -ChefServerUrl and -ValidationClientName options.");
+            }
         }
 
         protected override void ProcessRecord()
         {
             base.ProcessRecord();
-            // ToDo
+            ExecuteCommand();
         }
     }
 }
