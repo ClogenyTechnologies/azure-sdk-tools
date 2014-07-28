8e1414959548e6d24cdca4790e5f1a21736c968b
siddheshwar-more <siddheshwar-more@clogeny.com>
2014-07-28 15:43:38 +0000

    First draft for get chef extension cmdlet unit tests

---
+++ src/ServiceManagement/Compute/Commands.ServiceManagement.Test/UnitTests/Cmdlets/IaaS/Extensions/GetAzureVMChefExtensionCommandTests.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Test.UnitTests.Cmdlets.IaaS.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Commands.ServiceManagement.IaaS;
+    using System.Net;
+    using Commands.Test.Utilities.Common;
+    using Management.Compute;
+    using Management.Compute.Models;
+    using ServiceManagement.IaaS.Extensions;
+    using VisualStudio.TestTools.UnitTesting;
+    using Microsoft.WindowsAzure.Commands.Utilities;
+    using System.Management.Automation;
+    using Model;
+
+    // TODO: abstract out for other cmdlets
+    public class MockIPersistentVMForChefExtension : IPersistentVM
+    {
+        public PersistentVM GetInstance()
+        {
+            Model.PersistentVMModel.ResourceExtensionReference extensionRef = null;
+            extensionRef = new Model.PersistentVMModel.ResourceExtensionReference()
+            {
+                Name = "ChefClient",
+                Publisher = "Chef.Bootstrap.WindowsAzure,",
+                Version = "11.0"
+            };
+
+            var resourceList = new Model.PersistentVMModel.ResourceExtensionReferenceList();
+            resourceList.Add(extensionRef);
+
+            return new PersistentVM()
+            {
+                ResourceExtensionReferences = resourceList,
+                OSVirtualHardDisk = new Model.PersistentVMModel.OSVirtualHardDisk() { OS = "Windows" }
+            };
+        }
+    }
+
+
+    [TestClass]
+    public class GetAzureVMChefExtensionCommandTests : TestBase
+    {
+        private MockCommandRuntime mockCommandRuntime;
+        private MockIPersistentVMForChefExtension mockIPersistentVM;
+        [TestInitialize]
+        public void SetupTest()
+        {
+            mockCommandRuntime = new MockCommandRuntime();
+            mockIPersistentVM = new MockIPersistentVMForChefExtension();
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
+        }
+        public class GetAzureVMChefExtensionCommandStub : GetAzureVMChefExtensionCommand
+        {
+            public GetAzureVMChefExtensionCommandStub() { }
+        }
+
+        [TestMethod]
+        public void GetAzureVMChefExtensionExecuteChefCommand()
+        {
+            var getChefExtension = new GetAzureVMChefExtensionCommandStub()
+            {
+                CommandRuntime = mockCommandRuntime,
+                VM = mockIPersistentVM
+            };
+
+            getChefExtension.ExecuteCommand();
+
+            Assert.AreEqual(1, mockCommandRuntime.OutputPipeline.Count, "One item should be in the output pipeline");
+        }
+    }
+}
